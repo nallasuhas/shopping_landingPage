e13fd91e9f755c0e37cfd5a641f55d22c7e99767
nallasuhas <suhasnalla43@gmail.com>
2024-07-11 13:30:48 +0530

    laid a basic overall layout

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+        <main className='relative'>
+          nav
+            <section className='xl:padding-l wide:padding-r padding-b '>
+                hero
+            </section>
+            <section className='padding'>
+               PopularProducts 
+            </section>
+            <section className='padding'>
+              super quality
+            </section>
+            <section className='padding-x py-10'>
+                     services
+            </section>
+           <section className='padding'>
+                special offers
+            </section>
+            <section className='bg-pale-blue padding'>
+              CustomerReviews 
+            </section>
+            <section className='padding-x sm:py-32 py-16 w-full'>
+              Subscribe
+            </section>
+            <section className=' bg-black padding-x padding-t pb-8'>
+              Footer 
+            </section>
+       
+        </main>
+    </>
+  )
+}
+
+export default App
