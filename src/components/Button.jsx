8c3b32a6afa7136ae2ef2b47e5131819c1b29fe9
nallasuhas <suhasnalla43@gmail.com>
2024-07-12 13:43:49 +0530

    added subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Button({label, iconURL, backgroundColor="bg-coral-red ", borderColor, textColor}) {
+export default function Button({label, iconURL, backgroundColor="bg-coral-red ", borderColor, textColor, fullWidth}) {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${backgroundColor} ${borderColor} ${textColor}` }>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${backgroundColor} ${borderColor} ${textColor} ${fullWidth && "w-full"} rounded-full ` }>
       {label}
      {iconURL &&  <img src={iconURL} 
       className='ml-2 rounded-full w-5 h-5'/> }
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Button from "../components/Button"
 
 export default function Subscribe() {
   return (
-    <div>Subscribe</div>
+    // Subscribe section with width as max-container, flex container
+    <section id='contact-us'  className='max-container flex justify-between items-center max-lg:flex-col gap-10' >
+      {/* flex item */}
+      <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>
+        Sign Up for
+        <span className='text-coral-red'> Updates </span>& Newsletter
+      </h3>
+      {/* flex item, flex container */}
+      <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
+        {/* flex item, input field */}
+        <input type='text' placeholder='subscribe@nike.com' className='input' />
+        {/* flex item, flex container*/}
+        <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+          <Button label='Sign Up' fullWidth />
+        </div>
+      </div>
+
+    </section>
   )
 }
