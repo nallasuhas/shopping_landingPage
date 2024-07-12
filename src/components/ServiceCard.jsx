337c896ccb85df543588838bce22792eb2412c6b
nallasuhas <suhasnalla43@gmail.com>
2024-07-12 12:44:21 +0530

    added services section

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,19 @@
+
+
+export default function ServiceCard({imgURL,label, subtext}) {
+  return (
+    // div wrapper, servicecard, flex-item
+     <div className='flex-1 sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 ' >
+            {/* img wrapper div */}
+            <div className='w-12 h-12 flex justify-center items-center bg-coral-red rounded-full' >
+            <img src={imgURL} alt={label} width={25} height={25} />
+            </div>
+            <h3 className='mt-5 font-palanquin text-3xl leading-normal font-bold'>
+                {label}
+            </h3>
+            <p className='mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray'>
+                {subtext}
+            </p>
+     </div>
+  )
+}
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import {services} from '../constants/index'
+import ServiceCard from '../components/ServiceCard'
 
 export default function Services() {
   return (
-    <div>Services</div>
+    //  flex container , services section with width as max container
+      <section className='max-container flex justify-center flex-wrap gap-9'>
+         {/* service card */}
+          {services.map((service) => (
+           < ServiceCard key={service.label} {...service} />
+          ))}
+      </section>
   )
 }
